205fe56a17d14c2e57bd99641df5dac740e5c1b1
triquqd717 <actlikeuh@gmail.com>
2025-02-27 17:30:11 +0200

    Update oldservers.lua

--- modules/oldservers.lua
+++ modules/oldservers.lua
@@ -1,6 +1,6 @@
 local t = {
-  "a12b45ea-2849-49c5-afe4-2a929693eb90",
-  "920b72cd-5717-4d05-bbc3-1c5a34eef9c6",
+  ["a12b45ea-2849-49c5-afe4-2a929693eb90"] = "Old Server 1",
+  ["920b72cd-5717-4d05-bbc3-1c5a34eef9c6"] = "Old Server 2,
 }
 
 return t
