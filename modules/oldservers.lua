bc012e8a8a0f919599dd6a9b72045e32201896cf
triquqd717 <actlikeuh@gmail.com>
2025-02-27 14:40:11 +0200

    Update oldservers.lua

--- modules/oldservers.lua
+++ modules/oldservers.lua
@@ -1,6 +1,7 @@
 local t = {
   "a12b45ea-2849-49c5-afe4-2a929693eb90",
-  "193033a1-d999-4a1e-8b4a-54a0f1df2249"
+  "193033a1-d999-4a1e-8b4a-54a0f1df2249",
+  "920b72cd-5717-4d05-bbc3-1c5a34eef9c6"
 }
 
 return t
