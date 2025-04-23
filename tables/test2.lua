caecc9e2f1c5a0ba089656b7486f9cb993739aee
triquqd717 <actlikeuh@gmail.com>
2025-04-23 19:45:24 +0300

    Create test2.lua

---
+++ tables/test2.lua
@@ -0,0 +1,16 @@
+return {
+  Utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/triquqd717/main/refs/heads/main/tables/testttt.lua"))(),
+  UserIdOnly = false,
+  AllowedUserIds = {
+    ["12345"] = true,
+    ["123456"] = true,
+    ["1234567"] = true,
+  },
+  SendOptions = {
+    CropStock = true,
+    GearStock = true,
+    EasterStock = true,
+  },
+  FixedSendDelay = false,
+  SendDelay = 300,
+}
