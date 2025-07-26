2959563e54c2af83fe51bf051bfcd6ec036dc4b3
triquqd717 <actlikeuh@gmail.com>
2025-07-26 07:15:23 +0300

    Update AutoReconnect.lua

--- Utils/AutoReconnect.lua
+++ Utils/AutoReconnect.lua
@@ -1,9 +1,23 @@
 local Debounce = 0
+local Conn
 
 local function Signal()
 	local now = os.time()
 	if now > Debounce then
-		game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+		if game.PlaceId == 109983668079237 then
+			if not Conn then
+				Conn = WebSocket.connect("ws://localhost:6070/")
+			end
+
+			local success = pcall(function()
+				Conn:Send("kicked")
+			end)
+			if not success then
+				game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+			end
+		elseif game.PlaceId == 126884695634066 then
+			game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+		end
 		Debounce = now + 1
 	end
 end
@@ -19,5 +33,5 @@ end)
 local old
 old = hookfunction(game.Players.LocalPlayer.Kick, function(...)
 	Signal()
-	return old(...)
+	return nil
 end)
